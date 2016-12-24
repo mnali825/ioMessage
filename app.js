ecec122b8c47ecc53315eec8242e64c0e65baa9d
mir-aclewhip <mna271@nyu.edu>
2016-12-24 15:27:45 -0500

    importing project

---
+++ app.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+var debug = require('debug')('projectname');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  console.log(socket.id, 'has connected!');
+
+  socket.on('disconnect', function(socket) {
+    console.log(socket.id, 'see ya later');
+  });
+
+  socket.on('chat message', function(msg, usr) {
+    console.log('got message: ' +  msg + ' from ' + usr);
+    io.sockets.emit('chat message', msg, usr);
+  });
+
+  socket.on('mouse', function(data) {
+    // might be nice to see who's moving
+    data.id = socket.id;
+    console.log('other mouse', data);
+    //socket.broadcast.emit('other mouse', data);
+  });
+});
